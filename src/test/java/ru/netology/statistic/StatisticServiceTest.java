4186980172bb72db6e0e0e889b6c1e92e3bf3013
iojuch <petrovich8384@mail.ru>
2023-09-12 00:18:46 +0300

    JaCoCo added

--- src/test/java/ru/netology/statistic/StatisticServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticServiceTest.java
@@ -15,4 +15,16 @@ public class StatisticServiceTest {
 
         Assertions.assertEquals(expected, actual);
     }
+
+    @Test
+    void findAboveMax() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {12, 5, 8, 4, 15, 3, 8, 6, 11, 11, 12};
+        long expected = 15;
+
+        long actual = service.findMax(incomesInBillions);
+
+        Assertions.assertEquals(expected, actual);
+    }
 }
